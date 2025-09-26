d283cc5b943c67aec9058a2c6c433c1e3a6305c2
E-E-E-E-E-EE-EE-Ee <sburch@ftstudent.org>
2025-09-26 10:55:30 -0400

    Update osc.js

--- osc.js
+++ osc.js
@@ -1,22 +1,18 @@
-// Create an AudioContext
-const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+const audioContext = new AudioContext();
 
-// Create an oscillator node
+// Create an oscillator
 const oscillator = audioContext.createOscillator();
+oscillator.type = 'sine';
+oscillator.frequency.value = 440;
 
-// Set the wave type (e.g., 'sine', 'square', 'sawtooth', 'triangle')
-oscillator.type = 'sawtooth'; 
+// Create a filter node
+const filter = audioContext.createBiquadFilter();
+filter.type = 'lowpass'; // Filter out higher frequencies
+filter.frequency.value = 1000;
 
-// Set the frequency of the sound in Hz (e.g., 440 Hz for A4)
-oscillator.frequency.setValueAtTime(440, audioContext.currentTime);
+// Connect the nodes in a chain
+oscillator.connect(filter);
+filter.connect(audioContext.destination);
 
-// Connect the oscillator to the audio output (speakers)
-oscillator.connect(audioContext.destination);
-
-// Start the oscillator
+// Start the sound
 oscillator.start();
-
-// Stop the oscillator after a certain duration (e.g., 2 seconds)
-// Note: You cannot call start() again on a stopped oscillator.
-// To play the sound again, you need to create a new oscillator.
-oscillator.stop(audioContext.currentTime + 2); 
